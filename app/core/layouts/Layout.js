9daf65cf4fedc29012d48367b3fd45994c351222
Blitz.js CLI <noop@blitzjs.com>
2021-11-24 13:03:30 -0500

    Brand new Blitz app!

---
+++ app/core/layouts/Layout.js
@@ -0,0 +1,16 @@
+import { Head } from "blitz"
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || "ratemyfansv2"}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {children}
+    </>
+  )
+}
+
+export default Layout
